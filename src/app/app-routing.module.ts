90e8d97c9eabb1817e865e509679a6a9e2ce458f
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2024-03-23 16:09:38 +0530

    working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ThemeSwitcherComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
