3b4e4bb6b9abc1df883c7e1b7ba38e834b1c37af
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2024-09-06 23:10:51 +0530

    added prod env

--- src/app/Core/application_constant/environment.prod.ts
+++ src/app/Core/application_constant/environment.prod.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  //baseUrl: "https://api.brainqy.com",
-  baseUrl: "http://localhost:8083",
+  production: true,
+  baseUrl: "https://api.brainqy.com",
+  //baseUrl: "http://localhost:8083",
   contextUrl: "/ytms",
   signupUrl: "/ytms/register",
   loginUrl: "/ytms/login"
