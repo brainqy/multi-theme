5a839288da989396f833ab93f126395eea8d3dc8
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2024-11-04 09:03:49 +0530

    quiz store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -88,8 +88,6 @@ import { ResumeFormComponent } from './Component/resume-manager/resume-form/resu
 import { ResumesComponent } from './Component/resume-manager/resumes/resumes.component';
 import { BookmarksComponent } from './Component/bookmarks/bookmarks.component';
 import { environment } from './Core/application_constant/environment';
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { QuizStoreComponent } from './Component/test-prep/quiz-store/quiz-store.component';
 import { CreateQuizComponent } from './Component/test-prep/create-quiz/create-quiz.component';
 import { SelectQueComponent } from './Component/test-prep/select-que/select-que.component';
@@ -179,8 +177,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     QuillModule.forRoot(),
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    AngularFireAuthModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
     NgCircleProgressModule.forRoot({
       // set defaults here
       backgroundColor: "teal",
